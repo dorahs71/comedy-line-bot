151b94aace6b3fe9a63d0fe4e3da11defe4cedc6
Dora Hsieh <83153329+dorahs71@users.noreply.github.com>
2023-06-24 09:50:24 +0800

    feat: add send_city_activity function

--- respond.py
+++ respond.py
@@ -30,3 +30,26 @@ def send_recommend_activity(activity):
         recommend_message = TextSendMessage(text='唉呀...出了點問題耶～')
 
     return recommend_message
+
+
+def send_city_activity(city_activity_list):
+    if len(city_activity_list) == 1:
+        activity = city_activity_list[0]
+        send_recommend_activity(activity)
+    else:
+        if len(city_activity_list) > 6:
+            city_activity_list = city_activity_list[:6]
+
+        json_file = [generate_activity_json(
+            activity[-2], activity[0], activity[1], activity[2], activity[-1]) for activity in city_activity_list]
+        flex_contents = {"type": "carousel", "contents": json_file}
+
+        try:
+            city_message = FlexSendMessage(
+                alt_text='search_city_activities',
+                contents=flex_contents
+            )
+        except:
+            city_message = TextSendMessage(text='唉呀...出了點問題耶～')
+
+        return city_message
