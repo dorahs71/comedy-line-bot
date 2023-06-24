683308e5f58e3b1fc5d31309858b83e471ae0c99
Dora Hsieh <83153329+dorahs71@users.noreply.github.com>
2023-06-24 09:48:20 +0800

    feat: add new generate_city_dict function

--- data.py
+++ data.py
@@ -4,7 +4,19 @@ import random
 file = open('accupass_output.csv')
 reader = csv.reader(file)
 data_list = list(reader)
+data_list.pop(0)
 file.close()
+
+
+def generate_city_dict():
+    city_dict = {}
+
+    for row in data_list:
+        if row[1] not in city_dict.keys():
+            city_dict[row[1]] = [row]
+        else:
+            city_dict[row[1]].append(row)
+    return city_dict
 
 
 def random_recommend_activity():
